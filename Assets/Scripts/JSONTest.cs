8ad15fd602a89e4c00d0b2409d5f3af7324023ae
mr-jonix <submatukana@gmail.com>
2016-10-24 09:35:07 +0300

    Initial commit - import old project files

---
+++ Assets/Scripts/JSONTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class JSONTest : MonoBehaviour {
+
+    public TextAsset jsonSource;
+
+	// Use this for initialization
+	void Start () {
+        string json = jsonSource.text;
+        MTGSet TestSet = JsonUtility.FromJson<MTGSet>(json);
+        print(TestSet.name);
+        print(TestSet.translations.ru);
+        List<MTGCard> FilteredList = TestSet.cards.Where(x => x.types.Contains("Planeswalker")).ToList();
+        foreach(MTGCard card in FilteredList)
+        {
+            print(card.foreignNames[card.foreignNames.FindIndex(x => x.language =="Russian")].name + " at #"+card.number);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MTGSet.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+[Serializable]
+public class SetLanguages
+{
+    public string de;
+    public string fr;
+    public string it;
+    public string es;
+    public string pt;
+    public string jp;
+    public string cn;
+    public string tw;
+    public string ru;
+    public string ko;
+}
+
+[Serializable]
+public class ForeignCard
+{
+    public string language;
+    public string name;
+    public int multiverseid;
+}
+
+[Serializable]
+public class MTGSet {
+    public string name;
+    public string code;
+    public string magicCardsInfoCode;
+    public string releaseDate;
+    public string border;
+    public string type;
+    public string block;
+    public SetLanguages translations;
+    public List<MTGCard> cards;
+}
+
+[Serializable]
+public class MTGCard
+{
+    public string artist;
+    public float cmc;
+    public List<string> colorIdentity;
+    public List<string> colors;
+    public string flavor;
+    public List<ForeignCard> foreignNames;
+    public int hand;
+    public string id;
+    public string imageName;
+    public string layout;
+    public int life;
+    public int loyalty;
+    public string manaCost;
+    public string multiverseid;
+    public string name;
+    public List<string> names;
+    public string number;
+    public string originalText;
+    public string originalType;
+    public string power;
+    public List<string> printings;
+    public string rarity;
+    public bool reserved;
+    public string releaseDate;
+    public string text;
+    public bool timeshifted;
+    public string toughness;
+    public string type;
+    public List<string> types;
+    public string source;
+    public bool starter;
+    public List<string> subtypes;
+    public List<string> supertypes;
+    public List<int> variations;
+    public string watermark;
+
+}
