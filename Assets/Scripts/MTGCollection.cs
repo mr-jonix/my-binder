1cb927d2e1a90c5a8b1db485b4eac247884aca4d
mr-jonix <submatukana@gmail.com>
2017-04-26 18:19:45 +0300

    Basis for Inventory

--- Assets/Scripts/MTGCollection.cs
+++ Assets/Scripts/MTGCollection.cs
@@ -1,6 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+public class MTGSingleLanguageEntry
+{
+    public string language;
+    private uint regularCount;
+    private uint foilCount;
+    public string notes;
+
+    public MTGSingleLanguageEntry()
+    {
+        language = "en";
+        regularCount = 0;
+        foilCount = 0;
+        notes = string.Empty;
+    }
+}
+
+public class MTGInventoryEntry
+{
+    public string multiverseId;
+    public Dictionary<string, MTGSingleLanguageEntry> languageEntries;
+
+    public MTGInventoryEntry(string mId)
+    {
+        multiverseId = mId;
+        languageEntries = new Dictionary<string, MTGSingleLanguageEntry>();
+        languageEntries.Add("en",new MTGSingleLanguageEntry());
+    }
+}
 
 public class MTGCollection {
+
+    public Dictionary<string, MTGInventoryEntry> inventory;
 
 }
