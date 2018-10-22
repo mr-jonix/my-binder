e8c62254468e5b59381982707c4f9635e84529ba
mr-jonix <submatukana@gmail.com>
2018-10-22 19:02:19 +0300

    some settings

---
+++ Assets/AppQuitTemp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppQuitTemp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void QuitAppTemp()
+    {
+        Application.Quit();
+    }
+}
